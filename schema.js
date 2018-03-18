f3b935c7f42801158eda3663dc2e623c28dad088
Quan Truong <qmtruong92@gmail.com>
2018-03-18 10:10:08 -0700

    Ignore node_modules

--- schema.js
+++ schema.js
@@ -2,10 +2,12 @@ const fetch = require('node-fetch')
 // xml2js is an old tool that uses callbacks instead of promises
 // use util to make xml2js into a prmise
 const util = require('util')
+require('util.promisify').shim()
 const parseXML = util.promisify(require('xml2js').parseString)
 
 const x = fetch(
     'https://www.goodreads.com/author/show.xml?id=4432&key=847J7cQqCf38AFoS9oZng'
 ).then(response => response.text())
+    .then(parseXML)
 
 x
