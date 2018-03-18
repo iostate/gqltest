f1854eab4c2692eecd91afe8f06055b4e09e16ae
Quan Truong <qmtruong92@gmail.com>
2018-03-18 09:17:04 -0700

    parseXML

--- schema.js
+++ schema.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch')
 // xml2js is an old tool that uses callbacks instead of promises
 // use util to make xml2js into a prmise
 const util = require('util')
-util.promisify(require('xml2js').parseString)
+const parseXML = util.promisify(require('xml2js').parseString)
 
 const x = fetch(
     'https://www.goodreads.com/author/show.xml?id=4432&key=847J7cQqCf38AFoS9oZng'
