ca473694e27a056cd922ff444ec28a82602819b9
Quan Truong <qmtruong92@gmail.com>
2018-03-18 08:42:30 -0700

    Added express-graphql and implemented it in app

--- serve.js
+++ serve.js
@@ -1,6 +1,10 @@
 const express = require('express')
-
+const grapghqlHTTP = require('express-graphql')
 const app = express()
+
+app.use('/graphql', grapghqlHTTP({
+    graphiql: true
+}))
 
 app.listen(4000)
 
