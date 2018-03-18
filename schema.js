19ebfdf63d97bbbbe85108f69065a5549c6d1da1
Quan Truong <qmtruong92@gmail.com>
2018-03-18 09:16:31 -0700

    Added xml2js and util package to promisify xml2js

--- schema.js
+++ schema.js
@@ -1,4 +1,8 @@
 const fetch = require('node-fetch')
+// xml2js is an old tool that uses callbacks instead of promises
+// use util to make xml2js into a prmise
+const util = require('util')
+util.promisify(require('xml2js').parseString)
 
 const x = fetch(
     'https://www.goodreads.com/author/show.xml?id=4432&key=847J7cQqCf38AFoS9oZng'
